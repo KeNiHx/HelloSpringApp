be019696ba5ec89ae88ae5bc363bdd48da8fc229
KeNiHx <kennyhuylam@gmail.com>
2021-04-27 19:10:15 -0600

    created setter methods for class injections and configured the injection in spring config file

--- src/com/klam/springdemo/CricketCoach.java
+++ src/com/klam/springdemo/CricketCoach.java
@@ -4,12 +4,43 @@ public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	//add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
 	//create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
 		
-	}	
+	}		
 	
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.print("CricketCoach: inside setter method -setEmailAddress ");
+		this.emailAddress = emailAddress;
+	}
+
+
+
+	public String getTeam() {
+		System.out.print("CricketCoach: inside setter method -setTeam");
+		return team;
+	}
+
+
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+
+
 	// our setter method 
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside setter method - setFortuneService");
